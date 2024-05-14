b9d2ef05631b772369126ead22ee81be3833ab36
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-05-14 11:02:20 +0200

    model class added

---
+++ src/main/java/org/example/restaurant_system/model/Dish.java
@@ -0,0 +1,87 @@
+package org.example.restaurant_system.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "dishes")
+public class Dish {
+  // Attributes
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "id", nullable = false)
+  private Long id;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false)
+  private  Double price;
+
+  @Column(nullable = false)
+  private Integer calories;
+
+  @Column(nullable = false)
+  private String ingredients;
+
+
+// Constructors
+  public Dish() {
+  }
+
+  public Dish(String name, Double price, Integer calories, String ingredients) {
+    this.name = name;
+    this.price = price;
+    this.calories = calories;
+    this.ingredients = ingredients;
+  }
+
+  public Dish(Long id, String name, Double price, Integer calories, String ingredients) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.calories = calories;
+    this.ingredients = ingredients;
+  }
+
+  // Getters and Setters
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public Integer getCalories() {
+    return calories;
+  }
+
+  public void setCalories(Integer calories) {
+    this.calories = calories;
+  }
+
+  public String getIngredients() {
+    return ingredients;
+  }
+
+  public void setIngredients(String ingredients) {
+    this.ingredients = ingredients;
+  }
+}
